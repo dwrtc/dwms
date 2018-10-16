98ffad69281921cb0053030ee4c72412394bf33b
Demian <demian@nine.ch>
2018-10-16 08:06:34 +0200

    refactor tests

--- src/test/kotlin/WebsocketTest.kt
+++ src/test/kotlin/WebsocketTest.kt
@@ -1,10 +1,10 @@
 import ch.hsr.dsl.dwrtc.signaling.ClientService
 import ch.hsr.dsl.dwrtc.signaling.WebsocketIdMessage
 import ch.hsr.dsl.dwrtc.util.findFreePort
+import ch.hsr.dsl.dwrtc.util.jsonTo
 import ch.hsr.dsl.dwrtc.websocket.WEBSOCKET_PATH
 import ch.hsr.dsl.dwrtc.websocket.WebsocketHandler
 import io.javalin.Javalin
-import io.javalin.json.JavalinJackson
 import io.kotlintest.TestCaseOrder
 import io.kotlintest.extensions.TestListener
 import io.kotlintest.matchers.string.shouldContain
@@ -24,37 +24,28 @@ class WebsocketTest : WordSpec(), TestListener {
     val app = Javalin.create().start(port)
     val service = ClientService()
     val wsUri = Uri.of("ws://localhost:$port$WEBSOCKET_PATH")
-
-
-    private inline fun <reified OutputType> jsonTo(jsonString: String) =
-        JavalinJackson.fromJson(jsonString, OutputType::class.java)
-
+    
     init {
         "the initial message" should {
             WebsocketHandler(app, service)
+            val client = WebsocketClient.blocking(wsUri)
+            val firstMessageString = client.received().take(1).toList().first().bodyString()
+
             "be accessible under the specified port" {
-                val client = WebsocketClient.blocking(wsUri)
-                val firstMessageString = client.received().take(1).toList().first().bodyString()
                 firstMessageString.shouldNotBeBlank()
             }
 
             "be a WebsocketIdMessage" {
-                val client = WebsocketClient.blocking(wsUri)
-                val firstMessageString = client.received().take(1).toList().first().bodyString()
                 firstMessageString.shouldContain("WebsocketIdMessage")
                 firstMessageString.shouldContain("id")
             }
 
             "have a correct id" {
-                val client = WebsocketClient.blocking(wsUri)
-                val firstMessageString = client.received().take(1).toList().first().bodyString()
                 val firstMessage = jsonTo<WebsocketIdMessage>(firstMessageString)
                 firstMessage.id.length.shouldBe(36)
             }
 
             "have the correct type" {
-                val client = WebsocketClient.blocking(wsUri)
-                val firstMessageString = client.received().take(1).toList().first().bodyString()
                 val firstMessage = jsonTo<WebsocketIdMessage>(firstMessageString)
                 firstMessage.type.shouldBe("WebsocketIdMessage")
             }
