3e1cc86d7e59475f0a773e11491c1afe7ee57ee3
Demian <demian@nine.ch>
2018-12-06 13:14:47 +0100

    undo

--- src/test/kotlin/WebSocketTest.kt
+++ src/test/kotlin/WebSocketTest.kt
@@ -1,17 +1,16 @@
 package test
 
 import ch.hsr.dsl.dwrtc.signaling.ClientService
+import ch.hsr.dsl.dwrtc.util.findFreePort
 import ch.hsr.dsl.dwrtc.util.jsonTo
 import ch.hsr.dsl.dwrtc.websocket.WEBSOCKET_PATH
 import ch.hsr.dsl.dwrtc.websocket.WebSocketHandler
 import ch.hsr.dsl.dwrtc.websocket.WebSocketIdMessage
 import io.javalin.Javalin
-import io.kotlintest.TestCaseConfig
 import io.kotlintest.TestCaseOrder
 import io.kotlintest.extensions.TestListener
 import io.kotlintest.matchers.string.shouldContain
 import io.kotlintest.matchers.string.shouldNotBeBlank
-import io.kotlintest.seconds
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
 import mu.KLogging
@@ -21,10 +20,9 @@ import org.http4k.core.Uri
 class WebSocketTest : WordSpec(), TestListener {
     companion object : KLogging()
 
-    override val defaultTestCaseConfig = TestCaseConfig(invocations = 1, threads = 1, timeout = 5.seconds)
     override fun testCaseOrder() = TestCaseOrder.Random // make sure tests are not dependent on each other
 
-    private val port = 35571
+    private val port = findFreePort()
     private val app = Javalin.create().start(port)!!
     private val service = ClientService()
     private val wsUri = Uri.of("ws://localhost:$port$WEBSOCKET_PATH")
